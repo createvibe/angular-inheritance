8e5dfd7a25718c3bab156b0979118faca9898c47
Anthony Matarazzo <amatarazzo@marketshare.com>
2015-08-17 01:35:39 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # angular-inherits
 Classical Prototypal Inheritance For AngularJS
+
+NOTE: This library is still being developed.
+
+NOTE: Tests don't work yet, need to rebuild with karma for true angular tests
---
+++ test/class-only/index.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * (c) Anthony Matarazzo <email@anthonymatarazzo.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+'use strict';
+
+
+var Browser = require('zombie'),
+	expect = require('expect');
+
+// TODO: this is a quick and dirty test - clean it up and be more thorough
+
+describe('Classical Prototypal Inheritance:', function() {
+
+	var browser = new Browser();
+
+	before(function() {
+		return browser.visit('file://index.html');
+	});
+
+	describe('inherit class only', function() {
+
+		it('is an instance of SuperClass', function() {
+			browser.assert.evaluate('obj instanceof SuperClass');
+		});
+
+		it('is an instance of ParentClass', function() {
+			browser.assert.evaluate('obj instanceof ParentClass');
+		});
+
+		it('is an instance of ChildClass', function() {
+			browser.assert.evaluate('obj instanceof ChildClass');
+		});
+
+		it('called run on SuperClass', function() {
+			browser.assert.evaluate('obj._superCalled === true');
+		});
+
+	});
+
+});
